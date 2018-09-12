e7515d408e525b22c5fc56f15b0e9f8be6c9684d
Ellen Lee <leechinghui.tw@gmail.com>
2018-09-13 04:18:17 +0800

    validate inputs are positive number

--- js/add.js
+++ js/add.js
@@ -1,5 +1,5 @@
 function add (a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
+  if (typeof a !== 'number' || typeof b !== 'number' || a <= 0 || b <= 0) {
     return 'something wrong!!'
   }
   return a + b
--- test/add_spec.js
+++ test/add_spec.js
@@ -8,4 +8,12 @@ describe('function add', function () {
     var result = add('2', '1')
     result.should.be.equal('something wrong!!')
   })
+  it('0 is illegal', function () {
+    var result = add(0, 4)
+    result.should.be.equal('something wrong!!')
+  })
+  it('negative numbers are illegal', function () {
+    var result = add(-1, 4)
+    result.should.be.equal('something wrong!!')
+  })
 })
